3ec5a254e7f405928bff9c7d8774e026c9f19d97
farifi <muhdfarifi14@gmail.com>
2026-01-06 12:36:30 +0800

    add deleteStaff() function to call deleteStaff API

--- Front_end/KLGCC/src/apiContext.jsx
+++ Front_end/KLGCC/src/apiContext.jsx
@@ -15,8 +15,20 @@ export const StaffProvider = ({ children }) => {
         }
     };
 
+    const deleteStaff = async (staff_id) => {
+        try {
+            const res = await API.delete(`/api/staff/deleteStaff/${staff_id}`);
+            setStaffList( prev => prev.filter(
+                staff => staff.STAFF_ID !== staff_id 
+            ));
+            res.data.message;
+        } catch (err) {
+            alert("Failed to delete staff");
+        }
+    } 
+
     return (
-        <StaffContext.Provider value={{ staffList, fetchStaffList }}>
+        <StaffContext.Provider value={{ staffList, fetchStaffList , deleteStaff}}>
             {children}
         </StaffContext.Provider>
     );
