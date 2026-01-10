f0e84c3d9810fb3a168b887ac8387624c83c1898
farifi <muhdfarifi14@gmail.com>
2026-01-10 14:54:13 +0800

    added DashboardContext for dashboard api call

---
+++ Front_end/KLGCC/src/API Contexts Folder/DashboardContext.jsx
@@ -0,0 +1,43 @@
+import { useContext, useState, useEffect, createContext } from "react";
+import API from "../Api";
+
+const DashboardContext = createContext();
+
+const DashboardProvider = ({ children }) => {
+    const [dashboardData, setDashboardData] = useState({
+        bookingTrend: [],
+        bookingByCourse: [],
+        equimentUsage: [],
+        customerType: [],
+        staffByPosition: [],
+        multiLineBookings: [],
+        transactions: []
+    });
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchDashboardData = async () => { 
+            try {
+                const res = await API.get("/api/dashboard");
+                setDashboardData(res.data);
+            } catch (error) {
+                console.error("Dashboard API error", err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchDashboardData();
+    }, []);
+
+    return (
+        <DashboardContext.Provider value = {{ dashboardData, loading }}>
+            {children}
+        </DashboardContext.Provider>
+
+    );
+};
+
+export const useDashboard = () => {
+    return useContext(DashboardContext);
+};
